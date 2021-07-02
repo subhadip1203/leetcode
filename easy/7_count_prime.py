6f0b6ffdb98e82bd6512bdb3f27c00fa145456e7
subhadip1203 <subhadip1203@gmail.com>
2021-07-02 23:29:23 +0530

    easy : 7th-2nd solution

--- easy/7_count_prime.py
+++ easy/7_count_prime.py
@@ -2,18 +2,24 @@
 Link : https://leetcode.com/problems/count-primes/
 Count the number of prime numbers less than a non-negative number, n.
 '''
-
+import math 
 class Solution(object):
     def countPrimes(self, n):
-        if n <= 2:
+        if n<2:
             return 0
-        dp = [True] * n
-        dp[0] = dp[1] = False
-        for i in range(2, n):
-            if dp[i]:
-                for j in range(i*i, n, i):
-                    dp[j] = False
-        return sum(dp)
+        
+        #initialize a list of length n
+        prime=[1]*n
+		#mark 0th  and 1st index as 0
+        prime[0]=prime[1]=0
+        
+		#we will check for multiple from range 2 to sqrt(n)
+        for i in range(2,int(math.sqrt(n))+1):
+            if prime[i] == 1:
+			#mark all multiple of prime number as  0
+                prime[i*i:n:i] = [0] * ((n-1-i*i)//i + 1)
+        #return total count of prime 
+        return sum(prime)
 
 n=10
 print(Solution().countPrimes(n))
