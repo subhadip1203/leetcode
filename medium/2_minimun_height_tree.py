47c3eac8ea32c301a552f48ad273fc535052d13b
subhadip1203 <subhadip1203@gmail.com>
2021-08-25 21:02:13 +0530

    mid 2nd-2

--- medium/2_minimun_height_tree.py
+++ medium/2_minimun_height_tree.py
@@ -6,3 +6,6 @@ class Solution:
     def findMinHeightTrees(self, n, edges) :
         if n <= 2:
             return [x for x in range(n)]
+        else:
+            my_list=[]
+            return my_list
