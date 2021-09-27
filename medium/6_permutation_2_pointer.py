071581d6293e5252d03a41199d9d39119d3456e5
subhadip1203 <subhadip1203@gmail.com>
2021-09-28 00:18:42 +0530

    next permutation : 3nd part

--- medium/6_permutation_2_pointer.py
+++ medium/6_permutation_2_pointer.py
@@ -1,29 +1,21 @@
 '''
 https://leetcode.com/problems/next-permutation/
 '''
+
+
 class Solution():
-  def nextPermutation(self, nums):
-    i, j = len(nums)-2, len(nums)-1
+    def nextPermutation(self, nums):
+        left = len(nums)-2
+        while left >= 0 and nums[left] >= nums[left+1]:
+            left -= 1
+        right = left+1
+        while right < len(nums) and nums[right] > nums[left]:
+            right += 1
+        nums[left], nums[right-1] = nums[right-1], nums[left]
+        nums[left+1:] = sorted(nums[left+1:])
+        right -= 1
+        return nums
 
-    # first largest number in the left side
-    # and stop at smaller number
-    while i >= 0 and nums[i+1] <= nums[i]:
-      i -= 1 
 
-    # finding smaller number than i
-    if i >= 0: 
-      while j >= 0 and nums[j] <= nums[i]: 
-        j -= 1 # stop at larger number 
-			# swap 
-      nums[i], nums[j] = nums[j], nums[i]
-
-		# reverse the number between two indexes 
-    left, right = i+1, len(nums)-1
-    while left < right:
-      nums[left], nums[right] = nums[right], nums[left]
-      left += 1
-      right -= 1
-    return nums
-
-nums = [1,2,3]
+nums = [1, 2, 3]
 print(Solution().nextPermutation(nums))
