ed31682daf51e43b30df222d2cf15fbe27af3390
subhadip1203 <subhadip1203@gmail.com>
2021-10-01 23:20:38 +0530

    feat: add mid-7

---
+++ medium/7_jump_game.py
@@ -0,0 +1,14 @@
+'''
+https://leetcode.com/problems/jump-game/
+'''
+
+from typing import List
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        return True
+
+
+nums = [2, 3, 1, 1, 4]
+print(Solution().canJump(nums))
