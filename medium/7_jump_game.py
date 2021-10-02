91614776306845409822dafef44abf63348c62ae
subhadip1203 <subhadip1203@gmail.com>
2021-10-02 23:48:37 +0530

    fix: mid 7 Greedy algo

--- medium/7_jump_game.py
+++ medium/7_jump_game.py
@@ -7,7 +7,13 @@ from typing import List
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        return True
+        n = len(nums)
+        lastpos = n-1
+        for i in range(n-1, -1, -1):
+            if i + nums[i] >= lastpos:
+                lastpos = i
+
+        return lastpos == 0
 
 
 nums = [2, 3, 1, 1, 4]
