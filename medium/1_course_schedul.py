15d62d940c7f860d48a10ed6a89531f765365d0e
subhadip1203 <subhadip1203@gmail.com>
2021-10-13 23:19:45 +0530

    med 5: no of province

--- medium/1_course_schedul.py
+++ medium/1_course_schedul.py
@@ -5,36 +5,38 @@ video : https://www.youtube.com/watch?v=EgI5nU9etnU&t=371s
 learn : cycle detection algorithm
 """
 
+
 class Solution:
-    def canFinish(self, numCourses, prerequisites) :
-        initial_dict = {i:[] for i in range(numCourses)}
-        
-        for crs , pre in prerequisites :
+    def canFinish(self, numCourses, prerequisites):
+        initial_dict = {i: [] for i in range(numCourses)}
+
+        for crs, pre in prerequisites:
             initial_dict[crs].append(pre)
 
         visitedSet = set()
+
         def dfs(crs):
-            if crs in visitedSet :
+            if crs in visitedSet:
                 return False
             if initial_dict[crs] == []:
                 return True
             visitedSet.add(crs)
             for pre in initial_dict[crs]:
-                if dfs(pre) == False:
+                if dfs(pre) is False:
                     return False
-            visitedSet.remove(crs) 
-            initial_dict[crs]=[]      
+            visitedSet.remove(crs)
+            initial_dict[crs] = []
             return True
         for i in range(numCourses):
-            if dfs(i) == False:
+            if dfs(i) is False:
                 return False
         return True
 
 
 numCourses = 2
-prerequisites = [[1,0],[0,1]]
-print(Solution().canFinish(numCourses,prerequisites))
+prerequisites = [[1, 0], [0, 1]]
+print(Solution().canFinish(numCourses, prerequisites))
 
 numCourses = 2
-prerequisites = [[1,0]]
-print(Solution().canFinish(numCourses,prerequisites))
+prerequisites = [[1, 0]]
+print(Solution().canFinish(numCourses, prerequisites))
--- medium/5_number_of_provinces.py
+++ medium/5_number_of_provinces.py
@@ -3,3 +3,25 @@ Link : https://leetcode.com/problems/number-of-provinces/
 
 solution : https://www.youtube.com/watch?v=S5UUvCTM0V4
 '''
+
+
+from typing import List
+
+
+def findCircleNum(self, isConnected: List[List[int]]) -> int:
+    l = len(isConnected)
+    visited = [0]*l
+
+    def dfs(node):
+        visited[node] = True
+        for j in range(0, l):
+            if isConnected[node][j] == 1 and visited[j] is False:
+                dfs(j)
+
+    count = 0
+
+    for i in range(0, l):
+        if visited[i] is False:
+            count += 1
+            dfs(i)
+    return count
